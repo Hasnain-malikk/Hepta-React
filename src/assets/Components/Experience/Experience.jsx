1b8a06f0da1192b31bfda2ae9d0cf448df7976d8
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2024-05-12 22:08:21 +0500

    heading added

--- src/assets/Components/Experience/Experience.jsx
+++ src/assets/Components/Experience/Experience.jsx
@@ -5,7 +5,7 @@ const Experience = () => {
   return (
     <div className='exp'>
       <div className="top">
-        <div className="heading1"></div>
+        <div className="heading1">Experience Once In Your Life Time</div>
         <div className="paraa"></div>
       </div>
       <div className="bottom">
