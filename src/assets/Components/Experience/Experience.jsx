02e70673f145748e312f08df7d0501bb22ec015c
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2024-05-12 16:35:29 +0500

    outline added

---
+++ src/assets/Components/Experience/Experience.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./Experience.css"
+
+const Experience = () => {
+  return (
+    <div className='exp'>
+      <div className="top">
+        <div className="heading1"></div>
+        <div className="paraa"></div>
+      </div>
+      <div className="bottom">
+        <div className="block" id="a"></div>
+        <div className="block" id="b"></div>
+        <div className="block" id="c"></div>
+        <div className="block" id="d"></div>
+        <div className="block" id="e"></div>
+        <div className="block" id="f"></div>
+      </div>
+    </div>
+  )
+}
+
+export default Experience
--- src/assets/Pages/Home.jsx
+++ src/assets/Pages/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Hero from '../Components/Hero/Hero'
 import "./Home.css"
+import Experience from '../Components/Experience/Experience'
 
 const Home = () => {
   return (
     <div className='home'> 
       <Hero/>
+      <hr />
+      <Experience/>
     </div>
   )
 }
