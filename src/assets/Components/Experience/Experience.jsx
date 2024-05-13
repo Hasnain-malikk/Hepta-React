f1cedf4c99bf8abd124adfca42bf8f0b8a1b6236
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2024-05-13 10:17:26 +0500

    paraa updated

--- src/assets/Components/Experience/Experience.jsx
+++ src/assets/Components/Experience/Experience.jsx
@@ -6,7 +6,7 @@ const Experience = () => {
     <div className='exp'>
       <div className="top">
         <div className="heading1">Experience Once In Your Life Time</div>
-        <div className="paraa"></div>
+        <div className="paraa">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</div>
       </div>
       <div className="bottom">
         <div className="block" id="a"></div>
