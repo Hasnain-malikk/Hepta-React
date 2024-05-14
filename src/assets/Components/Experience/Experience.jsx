df9f2a2b29238bebaf593010dc4b40cba73615b6
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2024-05-14 18:53:53 +0500

    component added

--- src/assets/Components/Experience/Experience.jsx
+++ src/assets/Components/Experience/Experience.jsx
@@ -9,7 +9,12 @@ const Experience = () => {
         <div className="paraa">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</div>
       </div>
       <div className="bottom">
-        <div className="block" id="a"></div>
+        <div className="block" id="a">
+          <div className='img1'></div>
+          {/* <img src={require("../Media/.png")} alt="" /> */}
+          <div className="head1">Good Foods</div>
+          <div className="paraaa">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</div>
+        </div>
         <div className="block" id="b"></div>
         <div className="block" id="c"></div>
         <div className="block" id="d"></div>
