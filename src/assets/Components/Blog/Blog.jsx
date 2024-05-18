5b819199a01a18f785c1256ee55c544fe25b5337
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2024-05-18 16:54:07 +0500

    blog post added

--- src/assets/Components/Blog/Blog.jsx
+++ src/assets/Components/Blog/Blog.jsx
@@ -5,10 +5,14 @@ const Blog = () => {
   return (
     <div className='blog'>
       <div className="blog-post">
-        <div className="headingbp"></div>
-        <div className="parabp"></div>
+        <div className="headingbp"> <p> Recent Blog Post</p></div>
+        <div className="parabp">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</div>
       </div>
-      <div className="card"></div>
+      <div className="card">
+        <div className="card1"></div>
+        <div className="card2"></div>
+        <div className="card3"></div>
+      </div>
     </div>
   )
 }
