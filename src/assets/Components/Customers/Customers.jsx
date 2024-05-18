8f9d6d6a2f0d1069f3b841f4e86e6020a836c86d
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2024-05-18 21:37:46 +0500

    happy-customer completed

--- src/assets/Components/Customers/Customers.jsx
+++ src/assets/Components/Customers/Customers.jsx
@@ -1,14 +1,29 @@
 import React from 'react'
 import "./Customers.css"
+import p1 from "../Media/p1.webp"
+import p2 from "../Media/p2.webp"
+import p3 from "../Media/p3.webp"
 
 const Customers = () => {
   return (
     <div className='customer'>
       <div className="heading9">Happy Customers</div>
       <div className="review">
-        <div className="custom1"></div>
-        <div className="custom2"></div>
-        <div className="custom3"></div>
+        <div className="custom1">
+            <div className="cust-pic"><img src={p1} alt="" /></div>
+            <div className="cust-para">“Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.”</div>
+            <div className="cust-name">— Clare Gupta</div>
+        </div>
+        <div className="custom2">
+        <div className="cust-pic"><img src={p2} alt="" /></div>
+            <div className="cust-para">“Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.”</div>
+            <div className="cust-name">— Rogie Slater</div>
+        </div>
+        <div className="custom3">
+        <div className="cust-pic"><img src={p3} alt="" /></div>
+            <div className="cust-para">“Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.”</div>
+            <div className="cust-name">— John Doe</div>
+        </div>
       </div>
     </div>
   )
