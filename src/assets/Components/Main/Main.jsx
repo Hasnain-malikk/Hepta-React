96c580f9a1fc3fb426f64387d0f6baf3f28dfd5f
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2024-05-21 12:01:48 +0500

    main completed

--- src/assets/Components/Main/Main.jsx
+++ src/assets/Components/Main/Main.jsx
@@ -4,9 +4,9 @@ import "./Main.css"
 const Main = () => {
   return (
     <div className='main1'>
-      <div className="main-heading"></div>
-      <div className="main-para"></div>
-      <div className="main-btn"></div>
+      <div className="main-heading">Travel & Tours</div>
+      <div className="main-para">A free template by &nbsp;<a href="/">Colorlib</a>. Download and share!</div>
+      <div className="main-btn"><button class="transparent-button">VISIT COLORLIB</button></div>
     </div>
   )
 }
