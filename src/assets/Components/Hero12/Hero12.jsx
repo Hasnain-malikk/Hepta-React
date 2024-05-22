07b9d51308d2c0eea8b73d9be0ad525fb904f726
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2024-05-22 07:23:44 +0500

    paragraph completed

--- src/assets/Components/Hero12/Hero12.jsx
+++ src/assets/Components/Hero12/Hero12.jsx
@@ -5,7 +5,12 @@ const Hero12 = () => {
   return (
     <div className='hero12'>
       <div className="pic12"></div>
-      <div className="text12"></div>
+      <div className="text12">
+        <div className="heading12">Family Room</div>
+        <div className="para121">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</div>
+        <div className="para122">A small river named Duden flows by their place and supplies it with the necessary regelialia.</div>
+        <div className="btn12"><button class="transparent-button">LEARN MORE</button></div>
+      </div>
     </div>
   )
 }
--- src/assets/Components/Hero13/Hero13.jsx
+++ src/assets/Components/Hero13/Hero13.jsx
@@ -4,8 +4,8 @@ import "./Hero13.css"
 const Hero13 = () => {
   return (
     <div className='hero13'>
-      <div className="pic13"></div>
       <div className="text13"></div>
+      <div className="pic13"></div>
     </div>
   )
 }
