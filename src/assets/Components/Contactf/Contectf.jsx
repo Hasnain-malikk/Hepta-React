f5b12b0cbe3cafc3c05618563a94cf47c176f28e
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2024-05-24 23:36:37 +0500

    form added

--- src/assets/Components/Contactf/Contectf.jsx
+++ src/assets/Components/Contactf/Contectf.jsx
@@ -4,8 +4,20 @@ import "./Contectf.css"
 const Contectf = () => {
   return (
     <div className='contactf'>
-      <div className="form"></div>
-      <div className="info"></div>
+      <div className="form">
+        <div className="field1">
+          <div className="namee"></div>
+          <div className="phonee"></div>
+        </div>
+        <div className="field2"></div>
+        <div className="field3"></div>
+        <div className="btn46"></div>
+      </div>
+      <div className="info">
+        <div className="address"></div>
+        <div className="phone"></div>
+        <div className="email"></div>
+      </div>
     </div>
   )
 }
