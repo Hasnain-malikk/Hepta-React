06fd5b80754f1c10d634d0e4af8258b08d279205
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2024-05-24 08:31:39 +0500

    contact form added

---
+++ src/assets/Components/Contactf/Contectf.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Contectf.css"
+
+const Contectf = () => {
+  return (
+    <div className='contactf'>
+      <div className="form"></div>
+      <div className="info"></div>
+    </div>
+  )
+}
+
+export default Contectf
--- src/assets/Pages/Contact.jsx
+++ src/assets/Pages/Contact.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Main16 from '../Components/Main16/Main16'
+import Contectf from '../Components/Contactf/Contectf'
 
 const Contact = () => {
   return (
     <div>
       <Main16/>
+      <Contectf/>
     </div>
   )
 }
