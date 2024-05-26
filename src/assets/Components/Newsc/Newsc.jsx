6f94d944d5e4701b59e5d9de3b5ae1508af376e9
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2024-05-26 20:43:47 +0500

    popoular post side panel completed

--- src/assets/Components/Newsc/Newsc.jsx
+++ src/assets/Components/Newsc/Newsc.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import "./Newsc.css"
 import s1 from "../Media/s1.webp"
 import s2 from "../Media/s2.webp"
+import s6 from "../Media/s6.webp"
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const Newsc = () => {
   return (
@@ -58,8 +60,57 @@ const Newsc = () => {
         </div>
       </div>
       <div className="postf">
-        <div className="search12"></div>
-        <div className="popular"></div>
+        <div className="search12">
+
+      <div className="form-group">
+      <div className="input-container">
+        <input type="text" className="form-control search-input" placeholder="&nbsp;&nbsp;Search..." />
+        <span className="fa fa-search icon-search"></span>
+      </div>
+        </div>
+    </div>
+        <div className="popular">
+            <div className="pp">Popular Post</div>
+            <div className="main-mc">
+            <div className="mc">
+                <div className="imggg">
+                    <img src={s1} alt="" />
+                </div>
+                <div className="mainfff">
+                    <div className="dateee">FEBRUARY 26, 2018</div>
+                    <div className="headdd">Free Template by Colorlib</div>
+                </div>
+            </div>
+            <div className="mc"><div className="imggg">
+                    <img src={s2} alt="" />
+                </div>
+                <div className="mainfff">
+                    <div className="dateee">FEBRUARY 26, 2018</div>
+                    <div className="headdd">Free Template by Colorlib</div>
+                </div></div>
+            <div className="mc"><div className="imggg">
+                    <img src={s1} alt="" />
+                </div>
+                <div className="mainfff">
+                    <div className="dateee">FEBRUARY 26, 2018</div>
+                    <div className="headdd">Free Template by Colorlib</div>
+                </div></div>
+            <div className="mc"><div className="imggg">
+                    <img src={s2} alt="" />
+                </div>
+                <div className="mainfff">
+                    <div className="dateee">FEBRUARY 26, 2018</div>
+                    <div className="headdd">Free Template by Colorlib</div>
+                </div></div>
+            <div className="mc"><div className="imggg">
+                    <img src={s6} alt="" />
+                </div>
+                <div className="mainfff">
+                    <div className="dateee">FEBRUARY 26, 2018</div>
+                    <div className="headdd">Free Template by Colorlib</div>
+                </div></div>
+            </div>
+        </div>
         <div className="cat"></div>
       </div>
     </div>
