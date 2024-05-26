b8fa79beefac3d3438ae0eed6d3e8c764662a8fe
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2024-05-26 17:53:48 +0500

    card done

---
+++ src/assets/Components/Newsc/Newsc.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import "./Newsc.css"
+import s1 from "../Media/s1.webp"
+import s2 from "../Media/s2.webp"
+
+const Newsc = () => {
+  return (
+    <div className='newsc'>
+      <div className="cardf">
+        <div className="cardz">
+            <div className="cardz1">
+                <div className="pic120">
+                    <img src={s1} alt="" />
+                </div>
+                <div className="date120">FEBRUARY 26, 2018</div>
+                <div className="head120">Free Template by Colorlib</div>
+            </div>
+            <div className="cardz1">
+            <div className="pic120">
+                    <img src={s2} alt="" />
+                </div>
+                <div className="date120">FEBRUARY 26, 2018</div>
+                <div className="head120">Free Template by Colorlib</div>
+            </div>
+            <div className="cardz1">
+            <div className="pic120">
+                    <img src={s1} alt="" />
+                </div>
+                <div className="date120">FEBRUARY 26, 2018</div>
+                <div className="head120">Free Template by Colorlib</div>
+            </div>
+            <div className="cardz1">
+            <div className="pic120">
+                    <img src={s2} alt="" />
+                </div>
+                <div className="date120">FEBRUARY 26, 2018</div>
+                <div className="head120">Free Template by Colorlib</div>
+            </div>
+            <div className="cardz1">
+            <div className="pic120">
+                    <img src={s1} alt="" />
+                </div>
+                <div className="date120">FEBRUARY 26, 2018</div>
+                <div className="head120">Free Template by Colorlib</div>
+            </div>
+            <div className="cardz1">
+            <div className="pic120">
+                    <img src={s2} alt="" />
+                </div>
+                <div className="date120">FEBRUARY 26, 2018</div>
+                <div className="head120">Free Template by Colorlib</div>
+            </div>
+        </div>
+        <div className="wed">
+            <div className="s11">1</div>
+            <div className="s22">2</div>
+            <div className="s33">3</div>
+        </div>
+      </div>
+      <div className="postf">
+        <div className="search12"></div>
+        <div className="popular"></div>
+        <div className="cat"></div>
+      </div>
+    </div>
+  )
+}
+
+export default Newsc
--- src/assets/Pages/News.jsx
+++ src/assets/Pages/News.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Main15 from '../Components/Main15/Main15'
+import Newsc from '../Components/Newsc/Newsc'
 
 const News = () => {
   return (
     <div>
       <Main15/>
+      <Newsc/>
     </div>
   )
 }
