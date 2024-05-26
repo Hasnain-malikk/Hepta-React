d59afdbd446022c80eaa9e410c8b6fee97480986
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2024-05-26 09:54:06 +0500

    form adjusted

--- src/assets/Components/Contactf/Contectf.jsx
+++ src/assets/Components/Contactf/Contectf.jsx
@@ -1,43 +1,44 @@
-import React from 'react'
-import "./Contectf.css"
+import React from 'react';
+import './Contectf.css';
 
 const Contectf = () => {
   return (
-    <div className='contactf'>
+    <div className="contactf">
       <div className="form">
         <div className="field1">
-          <div className="namee">
-          <div className="ph2">Name</div>
-            <div className="ph1">
-              <input type="name" />
-            </div>
+          <div className="input-group">
+            <label className="ph2" htmlFor="name">Name</label>
+            <input className="ph1" type="text" id="name" name="name" placeholder="Enter your name" />
           </div>
-          <div className="phonee">
-          <div className="ph2">Phone</div>
-            <div className="ph1">
-              <input type="text" />
-            </div>
+          <div className="input-group">
+            <label className="ph2" htmlFor="phone">Phone</label>
+            <input className="ph1" type="tel" id="phone" name="phone" placeholder="Enter your phone" />
           </div>
         </div>
         <div className="field2">
-          <div className="ph2">Email</div>
-          <div className="ph1">
-            <input type="text" />
+          <div className="input-group">
+            <label className="ph2" htmlFor="email">Email</label>
+            <input className="ph1" type="email" id="email" name="email" placeholder="Enter your email" />
           </div>
         </div>
         <div className="field3">
-        <div className="ph2">Write Message</div>
-          <div className="ph1"><input type="text" /></div>
+          <form>
+            <label className="ph2" htmlFor="message">Message</label>
+            <textarea id="message" name="message" rows="4" placeholder="Type your message here"></textarea>
+            <input type="submit" value="Send Message" />
+          </form>
+          {/* <div className="message-box">
+            This is a message displayed in a box.
+          </div> */}
         </div>
-        <div className="btn46"></div>
       </div>
       <div className="info">
-        <div className="address"></div>
-        <div className="phone"></div>
-        <div className="email"></div>
+        <div className="address">Address: 123 Main St, Anytown, USA</div>
+        <div className="phone">Phone: (123) 456-7890</div>
+        <div className="email">Email: info@example.com</div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Contectf
+export default Contectf;
--- src/assets/Pages/Contact.jsx
+++ src/assets/Pages/Contact.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Main16 from '../Components/Main16/Main16'
 import Contectf from '../Components/Contactf/Contectf'
+import Customers from '../Components/Customers/Customers'
 
 const Contact = () => {
   return (
     <div>
       <Main16/>
       <Contectf/>
+      <Customers/>
     </div>
   )
 }
